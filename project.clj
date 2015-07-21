84715970cbb28d8a13c8caef1893445429390717
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-07-21 22:15:10 +0300

    updated deps for riemann 0.2.10

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject riemann-rabbitmq-plugin "0.1.0-SNAPSHOT"
   :description "A RabbitMQ plugin for Riemann"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.novemberain/langohr "3.1.0"]
+                 [com.novemberain/langohr "3.1.0" :exclusions [cheshire clj-http]]
                  ]
   :plugins [[codox "0.6.1"]
             [lein-midje "3.0.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [riemann "0.2.9"]
+                                  [riemann "0.2.10"]
                                   ]}}
   :codox {:src-linenum-anchor-prefix "L"
   :src-dir-uri "https://github.com/avishai-ish-shalom/riemann-rabbitmq-plugin/blob/master"})
